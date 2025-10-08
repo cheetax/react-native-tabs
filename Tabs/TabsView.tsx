9e9eacff9e6e05f86e6c6ec85d85631501a4fded
cheetax <dmitriy.grebenev@gmail.com>
2025-10-08 13:11:59 +0300

    081046 - 2

--- Tabs/TabsView.tsx
+++ Tabs/TabsView.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import { View } from "react-native"
 import { TouchableRipple, Text, Icon } from "react-native-paper"
 import { leftPadding } from "./Function"
